bbc63273f3312182fe19914bb70f2fe9511640c4
Fabian Frontczak <fabian.frontczak96@gmail.com>
2026-05-22 15:06:48 +0200

    Created java spring project with necessary dependencies

---
+++ src/main/java/org/dev/cashaccountsmanagerbackendapplication/CashAccountsManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dev.cashaccountsmanagerbackendapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashAccountsManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashAccountsManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev/cashaccountsmanagerbackendapplication/CashAccountsManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.cashaccountsmanagerbackendapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashAccountsManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
